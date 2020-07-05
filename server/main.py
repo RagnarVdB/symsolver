32b8d741a735437fde5ba34502749f629e112cc0
Ragnar Van den Broeck <ragnar.van.den.broeck@gmail.com>
2020-07-05 18:48:59 +0200

    vue router toegevoegd en klein testje voor server gemaakt

--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
+import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/integral/<string:integral>")
 @cross_origin()
-def test():
-   return jsonify(request.args.get('integral', default="test", type= str))
+def test(integral):
+  x = sp.symbols('x')
+  expr = parse_expr(integral)
+  I = sp.Integral(expr, x)
+  return jsonify(sp.latex(I.doit()))
