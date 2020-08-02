db3b06c0a5323de237ee2547cd1a7f6f40d8ec4f
Ragnar Van den Broeck <ragnar.van.den.broeck@gmail.com>
2020-08-02 16:03:49 +0200

    api endpoint integraal gemaakt

--- server/parser.py
+++ server/parser.py
@@ -1,9 +1,4 @@
-from sympy.parsing.sympy_parser import parse_expr
-
-def parser(integrand):
-  # expr = integrand.replace
+def parse(integrand):
+  from sympy.parsing.sympy_parser import parse_expr
+  expr = integrand
   return parse_expr(expr)
-
-# test cases
-print(parse('x^2'))
-print(parse('2x'))
