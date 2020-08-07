5c0abdc6d6b7cde315e50e6ba572571a5ea756ae
bjarnevergouwen <bjarne.vergouwen@student.kuleuven.be>
2020-08-07 14:31:39 +0200

    deel van parser gemaakt

--- server/parser.py
+++ server/parser.py
@@ -1,9 +1,17 @@
 from sympy.parsing.sympy_parser import parse_expr
 
 def parser(integrand):
+  symbols = ('+','-','*','/')
+  for elem in range(len(integrand)):
+    if integrand[elem] is '^':
+      integrand[elem] == '**'
+    elif isinstance(integrand[elem], int) and not isinstance(integrand[elem + 1], int):
+      if not isinstance(integrand[elem + 1], symbols) or integrand[elem + 1] is ' ':
+        integrand.insert('*',elem + 1)
+
   # expr = integrand.replace
-  return parse_expr(expr)
+  return integrand
 
 # test cases
-print(parse('x^2'))
-print(parse('2x'))
+print(parser('x^2'))
+print(parser('2x'))
